183e919bb90b7f4ba5c04f6814c95c534424c4ad
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-07-06 12:21:45 -0300

    feat - criação da funcionalidade do ex03

---
+++ ex03.php
@@ -0,0 +1,25 @@
+<?php
+
+    function mascararCPF($cpf){
+        $cpf = preg_replace("/\D/","", $cpf); // garante que tenha 11 caracteres
+
+        $cpfMascarado = substr_replace($cpf, "***.***.***-", 0, 9);// substitui os caracteres por * 
+        
+        return $cpfMascarado;
+    }
+
+    $cpfUsuario = "140.118.645-85";
+
+    $mascara = mascararCPF($cpfUsuario);
+
+
+    echo $cpfUsuario . "<br>";
+    echo $mascara;
+
+
+
+
+
+
+
+?>
