664cf22a37922e11f49d8340f89e45ff5f1e778d
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-07-06 18:25:57 -0300

    feat - criação da funcionalidade ex04

--- ex04.php
+++ ex04.php
@@ -1,9 +1,18 @@
 <?php
 
-Function gerarSenhas(){
+Function gerarSenhas($quantidade){
 
+    $caracteres = 'qwertyuiopasdfghjklçzxcvbnmQWERTYUIOPASDFGHJKLÇZXCVBNM0123456789!@#$%^&*()-_=+[]{}<>?/|'
+    $tamanho = strlen($caracteres);
 
+    for ($i = 0; $i < $quantidade; $i++){
+        $sortear = random_int(0, tamanho, -1); 
+        $senha .= $caracteres[$sortear];
+    } 
 
+    return $senha;
 }
 
 ?>
+
+echo gerarSenhas(12);
